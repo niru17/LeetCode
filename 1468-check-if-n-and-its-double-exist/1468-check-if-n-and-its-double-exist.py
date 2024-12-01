0df5eb32bf4a70f5bc9d87004226c10d1487c3d6
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2024-12-01 01:07:21 -0600

    Time: 0 ms (100%), Space: 17.4 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -2,7 +2,7 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         prevMap=set()
         for i in range(len(arr)):
-                if arr[i]*2 in prevMap or (arr[i]%2==0 and arr[i]/2 in prevMap):
+                if arr[i]*2 in prevMap or (arr[i]%2==0 and arr[i]//2 in prevMap):
                     return True
                 prevMap.add(arr[i])
         return False
